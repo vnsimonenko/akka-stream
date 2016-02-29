cc9836b8c1b778d5f901c2337b0a09f579486d2a
vns <you@example.com>
2016-02-29 15:04:10 +0200

    --

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 mvn clean package -DskipTests
-java -jar target/akka-java3.jar "data_100000.txt" "result1_1000.txt" "result2_1000.txt" "result3_1000.txt"
+java -jar target/akka-java.jar "data_100000.txt" "result1_1000.txt" "result2_1000.txt" "result3_1000.txt"
